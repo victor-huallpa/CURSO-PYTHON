80ba0781a77210298c109abad619f64b687077fe
victor-huallpa <vech.600@gmail.com>
2026-05-06 20:47:49 -0500

    fix: corriendo dominancia

--- estructuras_discretes/tabla_verdad/02_leyes_dominacion.py
+++ estructuras_discretes/tabla_verdad/02_leyes_dominacion.py
@@ -3,12 +3,18 @@
 #p^F ≡ F
 
 valores = [True, False]
-print("\tp\t|\tpvV\t|\tp^F")
+print("\tp\t|\tpvV\t|\tp^F\t|\tdominacion?")
 print("___________________________________________________")
 for p in valores:
-    if p == True or p == False: resul = True
-    if p != True or p != False: resul1 = False
-    print(f"\t{p}\t|\t{resul}\t|\t{resul1}")
+
+    #creamos disyuncion y conjuncion para las dominnancias
+    disyuncion = p or True
+    conjuncion = p and False
+    
+    #verifcamos si es dominacion
+    esIgual = (disyuncion == True) and (conjuncion == False)
+
+    print(f"\t{p}\t|\t{disyuncion}\t|\t{conjuncion}\t|\t{esIgual}")
 
 print(f"\nV ≡ pvV | F ≡ p^F\nPor ende son dominaicon")
 
